1cb9cdc77961f1317ab83f844915fec5c652d73e
Nishant Minerva <nishantcafe@gmail.com>
2023-09-20 17:38:29 +0530

    Initial Commit

---
+++ ToDoListSwiftUI/ToDoListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListSwiftUIApp.swift
+//  ToDoListSwiftUI
+//
+//  Created by Nishant Minerva on 20/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
